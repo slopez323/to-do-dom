8ce6deeb6ff27be4ded48e7db74c0bf5933be21d
Sarah Lopez <slopez323@gmail.com>
2022-03-29 13:48:55 -0400

    styled

--- main.js
+++ main.js
@@ -5,17 +5,32 @@ const delComp = document.querySelector('#delComp');
 const delAll = document.querySelector('#delAll');
 let count = 0;
 
+checkEmpty();
+
+function checkEmpty(){
+    if(!document.querySelector('.listItem')){
+        let empty = document.createElement('p');
+        empty.textContent = `Your list is empty.`;
+        listView.appendChild(empty);
+    };
+};
+
 addItem.addEventListener('submit', function (event) {
     event.preventDefault();
+
+    if(document.querySelector('p')) document.querySelector('p').remove();
 
     let newItem = document.createElement('div');
     let listActions = document.createElement('span');
     let deleteIcon = document.createElement('i');
     let completeIcon = document.createElement('i');
+    let editIcon = document.createElement('i');
 
+    editIcon.className = `fa fa-regular fa-pen-to-square edit-${count}`
     completeIcon.className = `fa fa-solid fa-check complete-${count}`
     deleteIcon.className = `fa fa-solid fa-trash-can delete-${count}`;
-    listActions.append(completeIcon, deleteIcon);
+    
+    listActions.append(editIcon, completeIcon, deleteIcon);
 
 
     newItem.className = 'listItem';
@@ -40,21 +55,53 @@ listView.addEventListener('click', function (event) {
     if (clicked[2] == 'fa-check') {
         completeItem(clicked);
     };
+
+    if (clicked[2] == 'fa-pen-to-square'){
+        editItem(clicked);
+    };
 });
 
 function deleteItem(clicked) {
     let number = (clicked[3].substring(clicked[3].search("-") + 1));
     let item = document.querySelector(`#item-${number}`);
     item.remove();
+    checkEmpty();
 };
 
 function completeItem(clicked) {
     let number = (clicked[3].substring(clicked[3].search("-") + 1));
     let item = document.querySelector(`#item-${number}`);
+    let comp = document.querySelector(`.complete-${number}`);
 
     if(item.classList.contains('completed')){
         item.classList.remove('completed');
-    } else item.classList.add('completed');
+        comp.style.color = '';
+    } else {
+        item.classList.add('completed');
+        comp.style.color = 'limegreen';
+    };
+};
+
+function editItem(clicked){
+    let number = (clicked[3].substring(clicked[3].search("-") + 1));
+    let item = document.querySelector(`#item-${number}`);
+    let span = item.querySelector('span');
+
+    let done = document.createElement('button');
+    done.className = 'btn';
+    done.textContent = 'Done';
+
+    item.append(done);
+    span.style.display = 'none';
+
+    item.contentEditable = true;
+    item.focus();
+
+    done.addEventListener('click', function(){
+        done.remove();
+        item.contentEditable = false;
+        span.style.display = '';
+    });
 };
 
 delComp.addEventListener('click', function(){
@@ -64,4 +111,5 @@ delComp.addEventListener('click', function(){
 
 delAll.addEventListener('click', function(){
     listView.innerHTML = '';
+    checkEmpty();
 });
